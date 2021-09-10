79fac3837718c1798364ef80e0078cfa6afb0f01
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2021-09-10 10:31:33 -0300

    Atualizando script.js

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-const newButton = document.getElementById('add__new');
-const cancelButton = document.getElementById('cancel');
-const sendButton = document.getElementById('send');
-const modal = document.getElementById('container__modal');
-const modalBackground = document.getElementById('bg');
+const newButton = document.getElementById("add__new");
+const cancelButton = document.getElementById("cancel");
+const sendButton = document.getElementById("send");
+const modal = document.getElementById("container__modal");
+const modalBackground = document.getElementById("bg");
 
-newButton.addEventListener('click', openNewModal);
-cancelButton.addEventListener('click', closeNewModal);
-modalBackground.addEventListener('click', closeNewModal);
+const buttonSim = document.getElementById("buttonSim");
+const buttonNao = document.getElementById("buttonNao");
+const modalExcluir = document.getElementById("modalExcluir");
+const buttonExcluir = document.getElementById("excluir");
+
+newButton.addEventListener("click", openNewModal);
+cancelButton.addEventListener("click", closeNewModal);
+buttonExcluir.addEventListener("click", abrirModalExcluir);
+buttonNao.addEventListener("click", fecharModalExcluir);
 
 function openNewModal() {
-  modal.style.display = 'flex';
+  modal.style.display = "flex";
 }
 
 function closeNewModal() {
-  modal.style.display = 'none';
+  modal.style.display = "none";
+}
+
+function abrirModalExcluir() {
+  modalExcluir.style.display = "flex";
+}
+
+function fecharModalExcluir() {
+  modalExcluir.style.display = "none";
 }
